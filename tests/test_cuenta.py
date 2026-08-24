cfb501d01674dfe3c63940f3b97ac693661f2245
LeoFloridia <floridialeandro@gmail.com>
2026-08-24 16:52:24 -0300

    test: agregar pruebas para cuentas y operaciones

---
+++ tests/test_cuenta.py
@@ -0,0 +1,116 @@
+from models.cuenta import Cuenta
+from models.moneda import Moneda
+from models.proposito_cuenta import PropositoCuenta
+
+
+def test_acreditar_monto_positivo():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.acreditar(50000)
+    
+    assert resultado is True
+    assert cuenta.saldo == 250000
+
+def test_acreditar_monto_negativo():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.acreditar(-50000)
+    
+    assert resultado is False
+    assert cuenta.saldo == 200000
+
+def test_acreditar_monto_cero():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.acreditar(0)
+    
+    assert resultado is False
+    assert cuenta.saldo == 200000
+
+def test_debitar_monto_positivo():
+    
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=150000
+    )
+    
+    resultado = cuenta.debitar(10000)
+    
+    assert resultado is True
+    assert cuenta.saldo == 140000
+    
+def test_debitar_saldo_insuficiente():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=50000
+    )
+    
+    resultado = cuenta.debitar(60000)
+    
+    assert resultado is False
+    assert cuenta.saldo == 50000
+
+def test_debitar_monto_cero():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.debitar(0)
+    
+    assert resultado is False
+    assert cuenta.saldo == 200000
+
+def test_debitar_monto_negativo():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.debitar(-70000)
+    
+    assert resultado is False
+    assert cuenta.saldo == 200000   
+
+def test_debitar_saldo_completo():
+
+    cuenta = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    resultado = cuenta.debitar(200000)
+    
+    assert resultado is True
+    assert cuenta.saldo == 0
---
+++ tests/test_operacion.py
@@ -0,0 +1,372 @@
+from models.cuenta import Cuenta
+from models.moneda import Moneda
+from models.proposito_cuenta import PropositoCuenta
+from models.operacion import Operacion
+from models.tipo_operacion import TipoOperacion
+from models.tipo_conversion import TipoConversion
+
+
+def test_ingreso():
+
+    mercado_pago = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    ingreso = Operacion(
+        fecha="13/08/2026",
+        tipo=TipoOperacion.INGRESO,
+        categoria="Ingreso",
+        descripcion="Cobro quincena",
+        monto=50000,
+        cuenta_origen=None,
+        cuenta_destino=mercado_pago
+    )
+    
+    resultado = ingreso.procesar()
+    
+    assert resultado is True
+    assert mercado_pago.saldo == 250000
+
+def test_gasto():
+
+    mercado_pago = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=200000
+    )
+    
+    gasto = Operacion(
+        fecha="13/08/2026",
+        tipo=TipoOperacion.GASTO,
+        categoria="Comida",
+        descripcion="Hamburguesas",
+        monto=20000,
+        cuenta_origen=mercado_pago,
+        cuenta_destino=None
+    )
+    
+    resultado = gasto.procesar()
+    
+    assert resultado is True
+    assert mercado_pago.saldo == 180000
+
+def test_gasto_saldo_insuficiente():
+
+    mercado_pago = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=10000
+    )
+    
+    gasto = Operacion(
+        fecha="13/08/2026",
+        tipo=TipoOperacion.GASTO,
+        categoria="Comida",
+        descripcion="Hamburguesas",
+        monto=20000,
+        cuenta_origen=mercado_pago,
+        cuenta_destino=None
+    )
+    
+    resultado = gasto.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 10000
+
+def test_transferencia():
+
+    mercado_pago = Cuenta(
+        nombre="Mercado Pago",
+        moneda=Moneda.ARS,
+        proposito=PropositoCuenta.DISPONIBLE,
+        saldo=100000
+    )
+    efectivo = Cuenta(
+            nombre="Billetera",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=10000
+    )
+    
+    transferencia = Operacion(
+        fecha="13/08/2026",
+        tipo=TipoOperacion.TRANSFERENCIA,
+        categoria="Transferencia",
+        descripcion="transferencia a efectivo",
+        monto=20000,
+        cuenta_origen=mercado_pago,
+        cuenta_destino=efectivo
+    )
+    
+    resultado = transferencia.procesar()
+    
+    assert resultado is True
+    assert mercado_pago.saldo == 80000
+    assert efectivo.saldo == 30000
+
+def test_transferencia_saldo_insuficiente():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=10000
+    )
+    efectivo = Cuenta(
+            nombre="Billetera",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=10000
+    )
+    
+    transferencia = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.TRANSFERENCIA,
+            categoria="Transferencia",
+            descripcion="transferencia a efectivo",
+            monto=20000,
+            cuenta_origen=mercado_pago,
+            cuenta_destino=efectivo
+    )
+    
+    resultado = transferencia.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 10000
+    assert efectivo.saldo == 10000
+
+def test_compra_dolares():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=100000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="compra de dolares",
+            monto=10,
+            cuenta_origen=mercado_pago,
+            cuenta_destino=ahorro,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.COMPRA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is True
+    assert mercado_pago.saldo == 85000
+    assert ahorro.saldo == 510
+
+def test_compra_dolares_saldo_insuficiente():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=100000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="compra de dolares",
+            monto=100,
+            cuenta_origen=mercado_pago,
+            cuenta_destino=ahorro,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.COMPRA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 100000
+    assert ahorro.saldo == 500
+
+def test_venta_dolares():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=100000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="venta de dolares",
+            monto=100,
+            cuenta_origen=ahorro,
+            cuenta_destino=mercado_pago,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.VENTA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is True
+    assert mercado_pago.saldo == 250000
+    assert ahorro.saldo == 400
+
+def test_venta_dolares_saldo_insuficiente():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=100000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="venta de dolares",
+            monto=1000,
+            cuenta_origen=ahorro,
+            cuenta_destino=mercado_pago,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.VENTA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 100000
+    assert ahorro.saldo == 500
+
+def test_compra_dolares_monedas_invalidas():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=100000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=50000
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="compra de dolares",
+            monto=10,
+            cuenta_origen=ahorro,
+            cuenta_destino=mercado_pago,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.COMPRA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 100000
+    assert ahorro.saldo == 50000
+
+def test_venta_dolares_monedas_invalidas():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.DISPONIBLE,
+            saldo=1000000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="venta de dolares",
+            monto=100,
+            cuenta_origen=mercado_pago,
+            cuenta_destino=ahorro,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.VENTA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 1000000
+    assert ahorro.saldo == 500
+
+def test_conversion_proposito_invalido():
+    
+    mercado_pago = Cuenta(
+            nombre="Mercado Pago",
+            moneda=Moneda.ARS,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=1000000
+    )
+    ahorro = Cuenta(
+            nombre="Exchange",
+            moneda=Moneda.USD,
+            proposito=PropositoCuenta.AHORRO,
+            saldo=500
+    )
+    
+    conversion = Operacion(
+            fecha="13/08/2026",
+            tipo=TipoOperacion.CONVERSION,
+            categoria="Conversion",
+            descripcion="venta de dolares",
+            monto=100,
+            cuenta_origen=mercado_pago,
+            cuenta_destino=ahorro,
+            precio_conversion=1500,
+            subtipo_conversion=TipoConversion.VENTA
+    )
+    
+    resultado = conversion.procesar()
+    
+    assert resultado is False
+    assert mercado_pago.saldo == 1000000
+    assert ahorro.saldo == 500
